fa9622902dbce4fb14bda1d56bc97309a9fcdf18
dmitriy1423 <01txxt10@gmail.com>
2022-10-27 20:47:19 +0700

    Небольшие изменения

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
-let modalContainer = document.querySelector(".modal-container");
-let navButton = document.querySelector(".button-nav");
-let searchHotel = document.querySelector(".button-search-hotel");
-let modalCloseButton = document.querySelector(".modal-close-button");
+const modalContainer = document.querySelector(".modal-container");
+const navButton = document.querySelector(".button-nav");
+const searchHotel = document.querySelector(".button-search-hotel");
+const modalCloseButton = document.querySelector(".modal-close-button");
 
-let wants =[navButton, searchHotel];
+const wants =[navButton, searchHotel];
 
 wants.forEach(item => item.addEventListener('click', (e) => {
   e.preventDefault();
-  e.stopPropagation();
   if (!(modalContainer.classList.contains("modal-container-open"))) {
     modalContainer.classList.add("modal-container-open");
   }
